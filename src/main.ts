412bf1e77606364768d173f0ddfc937c7a31c66a
Zetsukae <dangeloalexis2@gmail.com>
2026-07-08 12:15:48 +0200

    feat: add localStorage with JSON format

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,34 @@ const todoInput = document.getElementById('todo-input') as HTMLInputElement
 const todoElements = document.getElementById(
   'todo-elements',
 ) as HTMLUListElement
+const errorMessage = document.getElementById(
+  'error-message',
+) as HTMLParagraphElement
+
+let todos: string[] = []
+const savedTodos = localStorage.getItem('todos')
+if (savedTodos) {
+  todos = JSON.parse(savedTodos)
+}
+
+todos.forEach((todoText) => {
+  const li = document.createElement('li')
+  li.textContent = todoText
+  todoElements.appendChild(li)
+})
 
 addTodoButton.addEventListener('click', () => {
   const todoText = todoInput.value.trim()
-  const errorMessage = document.getElementById(
-    'error-message',
-  ) as HTMLParagraphElement
   if (todoText) {
     const li = document.createElement('li')
     li.textContent = todoText
     todoElements.appendChild(li)
+    todos.push(todoText)
+    localStorage.setItem('todos', JSON.stringify(todos))
+
     todoInput.value = ''
     errorMessage.textContent = ''
-  }
-  if (!todoText) {
+  } else {
     errorMessage.textContent = 'Please enter something to-do.'
   }
 })
