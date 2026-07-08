4a9e7a370c704a01e4c033096045aa33796d6fac
Zetsukae <dangeloalexis2@gmail.com>
2026-07-08 11:08:26 +0200

    feat: add basic logic for todo & styles

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,28 @@
 import './style.css'
 
 console.log('Hello from typescript')
+
+const addTodoButton = document.getElementById(
+  'add-todo-button',
+) as HTMLButtonElement
+const todoInput = document.getElementById('todo-input') as HTMLInputElement
+const todoElements = document.getElementById(
+  'todo-elements',
+) as HTMLUListElement
+
+addTodoButton.addEventListener('click', () => {
+  const todoText = todoInput.value.trim()
+  const errorMessage = document.getElementById(
+    'error-message',
+  ) as HTMLParagraphElement
+  if (todoText) {
+    const li = document.createElement('li')
+    li.textContent = todoText
+    todoElements.appendChild(li)
+    todoInput.value = ''
+    errorMessage.textContent = ''
+  }
+  if (!todoText) {
+    errorMessage.textContent = 'Please enter something to-do.'
+  }
+})
