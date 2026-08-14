22ada229a47b729100b8cd451b1dda1514337fb5
Zetsukae <dangeloalexis2@gmail.com>
2026-08-14 10:35:33 +0200

    feat: add checkbox with LocalStorage

--- src/main.ts
+++ src/main.ts
@@ -13,30 +13,64 @@ const errorMessage = document.getElementById(
   'error-message',
 ) as HTMLParagraphElement
 
-let todos: string[] = []
+interface Todo {
+  text: string
+  completed: boolean
+}
+
+let todos: Todo[] = []
 const savedTodos = localStorage.getItem('todos')
 if (savedTodos) {
   todos = JSON.parse(savedTodos)
 }
 
-todos.forEach((todoText) => {
+todos.forEach((todo) => {
   const li = document.createElement('li')
-  li.textContent = todoText
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  const todoSpan = document.createElement('span')
+  todoSpan.textContent = todo.text
+
+  checkbox.checked = todo.completed
+
+  li.appendChild(checkbox)
+  li.appendChild(todoSpan)
   todoElements.appendChild(li)
+
+  checkbox.addEventListener('change', () => {
+    todo.completed = checkbox.checked
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }) // [66] Bug IK : When create a todo and check it, and refresh, it doesn't save the state. We need to reload, recheck it and then it saves :ü
 })
 
 addTodoButton.addEventListener('click', () => {
   const todoText = todoInput.value.trim()
   if (todoText) {
     const li = document.createElement('li')
-    li.textContent = todoText
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    const todoSpan = document.createElement('span')
+    todoSpan.textContent = todoText
+
+    li.appendChild(checkbox)
+    li.appendChild(todoSpan)
     todoElements.appendChild(li)
-    todos.push(todoText)
-    localStorage.setItem('todos', JSON.stringify(todos))
 
     todoInput.value = ''
+
     errorMessage.textContent = ''
+    errorMessage.classList.remove('show')
+
+    todos.push({ text: todoText, completed: false })
+    localStorage.setItem('todos', JSON.stringify(todos))
+    window.location.reload() // [43] temporary fix for the bug, will be fixed in the next update
   } else {
-    errorMessage.textContent = 'Please enter something to-do.'
+    errorMessage.textContent =
+      "Please enter something to-do. You can't do nothing about your life. . ."
+    errorMessage.classList.add('show')
+    errorMessage.classList.remove('shake')
+    errorMessage.offsetWidth
+    errorMessage.classList.add('shake')
   }
 })
+// Rasiel was here ;} RK too :]
